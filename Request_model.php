d5554ee5208e2a55aa43ecfe94f07160748bdab4
cristian412 <cristianamarillacloss@gmail.com>
2017-04-14 18:19:02 -0400

    Update Request_model.php

--- Request_model.php
+++ Request_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Peticiones_model extends CI_Model {
+class Request_model extends CI_Model {
 
 	function __construct(){
 		parent::__construct();
