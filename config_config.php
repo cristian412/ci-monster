8de3715e0d15801bcaf7bb8879cc1c8e35b9246f
cristian412 <cristianamarillacloss@gmail.com>
2017-04-14 14:34:35 -0400

    Create config_config.php

---
+++ config_config.php
@@ -0,0 +1,13 @@
+	/*
+	|--------------------------------------------------------------------------
+	| MOST IMPORTANT GLOBAL VARIAABLES
+	|--------------------------------------------------------------------------
+	|
+	*/
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	$config["base_url"] = 'URL_VAR';
+	defined('URL')      OR define('URL', 'URL_VAR');
+	defined('APPNAME')  OR define('APPNAME', 'APPNAME_VAR');
+	defined('DATABASE') OR define('DATABASE', 'DATABASE_VAR');
+	defined('USER')     OR define('USER',     'USER_VAR');
+	defined('MBARETE')  OR define('MBARETE', 'MBARETE_VAR');
