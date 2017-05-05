6df202ae74f08ef725d2b607835a18f706367e0e
cristian412 <cristianamarillacloss@gmail.com>
2017-05-05 09:31:30 -0400

    Update and rename autoload.php to config_autoload.php

--- config_autoload.php
+++ config_autoload.php
@@ -1,9 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('session');
 $autoload['drivers'] = array();
-$autoload['helper'] = array('showhtml_helper');
+$autoload['helper'] = array('url','showhtml_helper');
 $autoload['config'] = array();
 $autoload['language'] = array();
 $autoload['model'] = array('Request_model','Tables_model');
+
