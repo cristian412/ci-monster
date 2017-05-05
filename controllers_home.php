a43363fda62cf8ed9570c66345bbac1977b4d469
cristian412 <cristianamarillacloss@gmail.com>
2017-05-05 09:52:43 -0400

    Update controllers_home.php

--- controllers_home.php
+++ controllers_home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 
 	public function index(){
 
-		$data['yield_data'] = $this->load->view('layout/adminlte/index','',true);
+		$data['yield_data'] = $this->load->view('home/index','',true);
 
 		$this->load->view('layout/adminlte/index',$data);
 	}
