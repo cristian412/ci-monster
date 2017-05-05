216a59f96cbdc5021913da8065a0fb4f4b400970
cristian412 <cristianamarillacloss@gmail.com>
2017-05-05 09:01:31 -0400

    Create controllers_home.php

---
+++ controllers_home.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index(){
+
+		$data['yield_data'] = $this->load->view('layout/adminlte/index',,true);
+
+		$this->load->view('layout/adminlte/index',$data);
+	}
+}
