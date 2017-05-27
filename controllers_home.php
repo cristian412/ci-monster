678d54d70ae959b1723b11b4bcfe4eda5af995d5
cristian412 <cristianamarillacloss@gmail.com>
2017-05-27 16:16:39 -0400

    Update controllers_home.php

--- controllers_home.php
+++ controllers_home.php
@@ -4,8 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	public function index(){
+		$data['tables'] = $this -> Tables_model -> show_tables();
 
-		$data['yield_data'] = $this->load->view('home/index','',true);
+		$data['yield_data'] = $this->load->view('home/index',$data,true);
 
 		$this->load->view('layout/adminlte/index',$data);
 	}
