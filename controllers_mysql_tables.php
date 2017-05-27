6c3f0d15c32a5a7c80131b7a25179fa8ee75cdd5
cristian412 <cristianamarillacloss@gmail.com>
2017-05-27 12:32:05 -0400

    Update controllers_mysql_tables.php

--- controllers_mysql_tables.php
+++ controllers_mysql_tables.php
@@ -9,15 +9,13 @@ class Tables extends CI_Controller {
 	}
 
 	public function index(){
-		$u = $this ->session->userdata('u');
-		if($u!=1) header('location:'.URL);
 		$data['data']['create_message'] = $this -> Tables_model -> create();
 		$data['data']['alter_message']  = $this -> Tables_model -> alter();
 		$data['data']['files_message']  = $this -> Tables_model -> files();
 		$data['data']['table_name']  = $this -> Tables_model -> table_name();
 		$data['data']['show_tables'] = $this -> Tables_model -> show_tables();
-		$data['yield'] = 'base/tables';
-	    $this->load->view('base/layout',$data);	
+		$data['yield_data'] = $this->load->view('mysql/index',$data);
+	    $this->load->view('layout/adminlte/index',$data);	
 	}
 
 }
