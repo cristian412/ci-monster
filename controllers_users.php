ad789c10b37f00054662b1149de273239db61a51
cristian412 <cristianamarillacloss@gmail.com>
2018-03-31 19:20:33 -0400

    Update controllers_users.php

--- controllers_users.php
+++ controllers_users.php
@@ -12,37 +12,76 @@ class Users extends CI_Controller {
 
 
 	public function index(){
-		$this->load->view('layout/adminlte/sign_in');
+
+		$data['login_url'] = $this->googleplus->loginURL();
+
+
+			
+		//$this->load->view('welcome_message',$data);
+
+		$this->load->view('layout/adminlte/sign_in',$data);
 	}
 	public function sign_in(){
+		$data['login_url'] = $this->googleplus->loginURL();
 		$data['message'] = '';
 		$this->load->view('layout/adminlte/sign_in',$data);
+
 	}
 	public function sign_up(){
 		$this->load->view('layout/adminlte/sign_up');
 	}
 	public function sign_out(){
 		$this->session->sess_destroy();
-		redirect( URL );
+		$this->googleplus->revokeToken();
+		redirect( URL.'users/sign_in' );
 	}
 
 	public function check(){
-		$usuario = $this->input->post('user');
-		$pass = $this->input->post('pass');
-		$q = "SELECT * from users where username='$usuario' and password='$pass'";
-		$r = $this->Request_model->peticion($q);
-		if( count($r)>0 ){
-			foreach ($r[0] as $key => $value)
-				$this->session->set_userdata($key, $value);
-			
-			$tables = $this -> Tables_model -> table_name();
-			$this->session->set_userdata('tables', $tables);
-			
-			redirect( URL );
-		}else{
-			$data['message'] = 'ERROR USER OR PASSWORD WRON';
-			$this->load->view('layout/adminlte/sign_in',$data);
+
+		if( $this->input->post('user') ){
+			$usuario = $this->input->post('user');
+			$pass = $this->input->post('pass');
+			$q = "SELECT * from users where username='$usuario' and password='$pass'";
+			$r = $this->Request_model->peticion($q);
+			if( count($r)>0 ){
+				foreach ($r[0] as $key => $value)
+					$this->session->set_userdata($key, $value);
+				
+				$tables = $this -> Tables_model -> table_name();
+				$this->session->set_userdata('tables', $tables);
+
+				$this->session->set_userdata('login',true);
+				
+				redirect( URL );
+			}else{
+				$data['message'] = 'ERROR USER OR PASSWORD WRON';
+				$this->load->view('layout/adminlte/sign_in',$data);
+			}
 		}
+
+		if (isset($_GET['code'])) {
+			$this->googleplus->getAuthenticate();
+			$this->session->set_userdata('user_profile',$this->googleplus->getUserInfo());
+			$email = $_SESSION['user_profile']['email'];
+
+			$q = "SELECT * from users where email='{$email}'";
+			$r = $this->Request_model->peticion($q);
+			if( count($r)>0 ){
+				foreach ($r[0] as $key => $value)
+					$this->session->set_userdata($key, $value);
+
+				$this->session->set_userdata('login',true);
+				$tables = $this -> Tables_model -> table_name();
+				$this->session->set_userdata('tables', $tables);
+				redirect( URL );
+			}else{
+				$data['message'] = 'ERROR EMAIL WRON';
+				$this->load->view('layout/adminlte/sign_in',$data);
+			}
+
+
+		}	
+
 	}
 	public function check_pru(){
 		$usuario = $this->input->post('user');
@@ -50,4 +89,18 @@ class Users extends CI_Controller {
 		$q = "SELECT * from users where username='$usuario' and password='$pass'";
 		echo $q;
 	}
+
+	// INICIA GOOGLE PLUS
+
+	public function profile(){
+		
+		if($this->session->userdata('login') != true){
+			redirect('');
+		}
+		
+		$contents['user_profile'] = $this->session->userdata('user_profile');
+		$this->load->view('profile',$contents);
+		
+	}
+	
 }
