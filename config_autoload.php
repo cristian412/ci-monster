6ae2ed1b81f92d553457e0cf675d6f14ec7e12f4
cristian412 <cristianamarillacloss@gmail.com>
2018-04-01 10:28:10 -0400

    Update config_autoload.php

--- config_autoload.php
+++ config_autoload.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
-$autoload['libraries'] = array('session','googleplus'); // edit library/Googleplus.php and unzip third_party folder
+$autoload['libraries'] = array('session'); // add ,'googleplus' edit library/Googleplus.php and unzip third_party folder
 $autoload['drivers'] = array();
 $autoload['helper'] = array('url','showhtml_helper');
 $autoload['config'] = array();
